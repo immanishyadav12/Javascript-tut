ebbd7dd8d8b8b7ceeb2a46f95912bdc6245a4e01
immanishyadav12 <121007976+immanishyadav12@users.noreply.github.com>
2023-10-13 17:30:06 +0000

    first

---
+++ js/first-js.js
@@ -0,0 +1,9 @@
+const accountId = 1245
+let emailId = "Hello@gamsil.com"
+var accountPassword = "Hellowme"
+
+//this is a comment
+
+console.log(accountId);
+console.table([accountId, emailId, accountPassword])
+
